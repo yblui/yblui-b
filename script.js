3cae40c5ed81251216f3049fdbe17eaa2749776e
y.b.l** <69856522+yblui@users.noreply.github.com>
2020-08-24 21:46:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
-var mmm = 0
+var mmm = 0;
 var num = document.getElementById("t");
-var jgl=false;
+var jgl = false;
 function calCulate(val) {
 	switch (val) {
 		case "=":
-			num.value = eval(num.value);
+			var wb="";
+			var lsval=num.value;
+			var calcval=num.value;
+			while (true){
+				var jg=lsval.search(/[\+\-\*\/].*?\%/);
+				if(jg!=-1){
+					wb=Number(lsval.slice(jg+1,lsval.search("%")-1))/100;
+					calcval=calcval.replace(lsval.slice(jg,lsval.search("%")),lsval[jg]+wb+"%")
+					lsval=lsval.replace(/[\+\-\*\/].*?\%/,"")
+				}
+				else{
+					break;
+				}
+			}
+			num.value = eval(calcval);
+			break;
+		case "<":
+			num.value = num.value.slice(0,-1);
 			break;
 		case "AC":
 			num.value = "";
 			break;
 		case "M+":
-			mmm += Number(eval(num.value))
+			mmm += Number(eval(num.value));
 			break;
 		case "MR":
-			num.value += mmm.toString()
+			num.value += mmm.toString();
 			break;
 		case "M-":
 			mmm -= Number(eval(num.value));
 			break;
 		case "MC":
-			mmm=0;
+			mmm = 0;
 			break;
 		case "+":
 		case "-":
